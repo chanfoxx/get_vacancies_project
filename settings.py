5d71796f1818977e9a2481545d9e114ef827fa63
Oh Olya <chanfoxx@yandex.ru>
2023-08-22 19:21:17 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+
+CURRENT_PATH = Path(__file__).parent
+PATH_FILE = Path.joinpath(CURRENT_PATH, 'src', 'vacancies.json')
+PATH_AREA_FILE = Path.joinpath(CURRENT_PATH, 'src', 'areas.json')
